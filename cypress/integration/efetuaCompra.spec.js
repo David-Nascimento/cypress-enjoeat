f4119d92c81ddd44959540b246395dd7d9b8118d
David Halison <halison700@gmail.com>
2021-04-16 23:10:09 -0300

    [CREATED] iniciado projeto com cypress

---
+++ cypress/integration/efetuaCompra.spec.js
@@ -0,0 +1,11 @@
+// <reference types="cypress" />
+
+describe('Vendas', () => {
+
+    it('deve mover um item para o carrinho', () => {
+        cy.visit('http://automationpractice.com/index.php?id_category=3&controller=category')
+       
+        
+    })
+
+})
